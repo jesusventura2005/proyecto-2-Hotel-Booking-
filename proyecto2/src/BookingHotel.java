78670f14ae655e4707725cb826a4d9808854ca90
jesusventura2005 <45541675+jesusventura2005@users.noreply.github.com>
2024-03-12 18:02:22 -0400

    prueba de leer excel

--- proyecto2/src/BookingHotel.java
+++ proyecto2/src/BookingHotel.java
@@ -3,42 +3,46 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.remote.DesiredCapabilities;
+
 import java.io.FileInputStream;
-import java.io.IOException;
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.Row;
-import org.apache.poi.xssf.usermodel.XSSFCell;
-import org.apache.poi.xssf.usermodel.XSSFRow;
-import org.apache.poi.xssf.usermodel.XSSFSheet;
-import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.usermodel.WorkbookFactory;
+
 
 /**
  *
  * @author jesus
  */
 public class BookingHotel {
+    
+   
 
-    public void read(String[] args) throws IOException {
-        FileInputStream file = new FileInputStream("c:\\Users\\jesus\\OneDrive\\Escritorio\\Booking_hotel.xlsx");
-        XSSFWorkbook workbook = new XSSFWorkbook(file);
-        Sheet sheet = workbook.getSheet("Sheet1");
-        Row row = sheet.getRow(0);
-        Cell cell = row.getCell(0);
-        System.out.println(sheet.getRow(0).getCell(0));
-        Row row1 = sheet.getRow(1);
-        Cell cell1 = row1.getCell(1);
-        System.out.println(sheet.getRow(0).getCell(1));
-        Row row2 = sheet.getRow(1);
-        Cell cell2 = row2.getCell(1);
-        System.out.println(sheet.getRow(1).getCell(0));
-        Row row3 = sheet.getRow(1);
-        Cell cell3 = row3.getCell(1);
-        System.out.println(sheet.getRow(1).getCell(1));
-//String cellval = cell.getStringCellValue();
-//System.out.println(cellval);
+
+
+    public String Read(String sheetName, int rNum , int cNum ) {
+        String data = "";
+        try {
+            FileInputStream fis = new FileInputStream("C:\\Java\\Booking_hotel.xlsx");
+            Workbook wb = WorkbookFactory.create(fis);
+            Sheet s = wb.getSheet(sheetName);
+            Row r = s.getRow(rNum);
+            Cell c = r.getCell(cNum);
+            data = c.getStringCellValue();
+            
+            
+        } catch (Exception e) {
+            System.out.println("error");
+            e.printStackTrace();
+        }
+        
+        return data;
+
     }
+    
+    
+
 
 }
-
--- proyecto2/src/Main.java
+++ proyecto2/src/Main.java
@@ -1,8 +1,8 @@
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-
 /**
  *
  * @author jesus
@@ -12,5 +12,17 @@ public class Main {
     /**
      * @param args the command line arguments
      */
+    public static void main(String[] args) {
+
+      
+
+        
+
+        BookingHotel obj = new BookingHotel();
+        String ci = obj.Read("reservas", 2, 0);
+
+        System.out.println(ci);
+
+    }
 
 }
